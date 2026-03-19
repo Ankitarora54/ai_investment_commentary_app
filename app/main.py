dbff669e284e1e16a5f4071922212ed09e344ac4
ankitarora54 <ankitarora54@live.com>
2026-03-19 18:35:07 +0530

    added streamlit st.secret

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ from openai import OpenAI
 import os
 
 load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
+#client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 st.title("AI Investment Commentary Generator")
 
 uploaded_file = st.file_uploader("Upload portfolio CSV", type=["csv"])
