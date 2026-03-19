d38b81fb08e93fdec11a63379abf23bebe525962
ankitarora54 <ankitarora54@live.com>
2026-03-19 22:10:03 +0530

    Selection Options added

--- app/main.py
+++ app/main.py
@@ -13,11 +13,16 @@ def load_css():
     with open("app/styles.css") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
+def get_sample_files():
+    sample_path = "data/sample"
+    files = [f for f in os.listdir(sample_path) if f.endswith(".csv")]
+    return files, sample_path
+
 load_css()
 
 load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-#client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+#client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 st.set_page_config(page_title="Investment Commentary Generator", layout="wide")
 st.title("Investment Commentary Generator")
 st.markdown("AI-Powered Investment Reporting Tool")
@@ -33,37 +38,55 @@ client_type = st.selectbox("Client Type", ["Retail", "Institutional"])
 
 if mode == "Generate Single Portfolio Summary":
     st.subheader("Generate Commentary for a Single Portfolio")
+    #st.markdown('<div class="section-card">', unsafe_allow_html=True)
+    st.subheader("Select Data Source")
+    data_source = st.radio("Choose input method:",["Use Sample File"
+                                                   , "Upload Your Own File"])
+    df = None
+    uploaded_file = None
+    selected_file = None
+    if data_source == "Use Sample File":
+        uploaded_file = None
+        files, sample_path = get_sample_files()
+        file_options = ["-- Select a file --"] + files
+        selected_file = st.selectbox("Select Sample File", file_options,index=0)
+        if selected_file != "-- Select a file --":
+            file_path = os.path.join(sample_path, selected_file)
+            df = pd.read_csv(file_path)
+            st.success(f"Loaded sample file: {selected_file}")
+    elif data_source == "Upload Your Own File":
+        selected_file = None
+        uploaded_file = st.file_uploader("Upload Portfolio CSV", type=["csv"])
+        if uploaded_file:
+            #selected_file = st.selectbox("Select Sample File", files)
+            #uploaded_file = st.file_uploader("Upload portfolio CSV", type=["csv"])
+            #period = st.selectbox("Select Reporting Period", ["Monthly", "Quarterly"])
+            df = pd.read_csv(uploaded_file)
+            st.subheader("Portfolio Data")
+            st.dataframe(df)
 
-    uploaded_file = st.file_uploader("Upload portfolio CSV", type=["csv"])
-
-    #period = st.selectbox("Select Reporting Period", ["Monthly", "Quarterly"])
-
-    if uploaded_file:
-        df = pd.read_csv(uploaded_file)
-
-        st.subheader("Portfolio Data")
-        st.dataframe(df)
-
+    if selected_file is not None and selected_file != "-- Select a file --" or uploaded_file is not None:
         # Chart
         st.subheader("Sector Performance")
         sector_chart = df.groupby('sector')['return'].mean()
         st.bar_chart(sector_chart)
 
-        if st.button("Generate AI Commentary"):
-            metrics = calculate_metrics(df)
-            benchmark_data = compare_with_benchmark(metrics['avg_return'])
-            with st.spinner("Generating AI commentary..."):
-                commentary = generate_commentary(metrics,benchmark_data,period
-                                            ,client_type,market_context="",client = client)
+    if st.button("Generate AI Commentary"):
+        metrics = calculate_metrics(df)
+        benchmark_data = compare_with_benchmark(metrics['avg_return'])
+        with st.spinner("Generating AI commentary..."):
+            commentary = generate_commentary(metrics,benchmark_data,period
+                                        ,client_type,market_context="",client = client)
 
-            st.subheader("Generated Commentary")
-            st.write(commentary)
+        st.subheader("Generated Commentary")
+        st.write(commentary)
 
-            # PDF download
-            pdf_file = generate_pdf(commentary)
-            with open(pdf_file, "rb") as f:
-                st.download_button("Download Report", f, file_name="commentary.pdf")
-        
+        # PDF download
+        pdf_file = generate_pdf(commentary)
+        with open(pdf_file, "rb") as f:
+            st.download_button("Download Report", f, file_name="commentary.pdf")
+    st.markdown('</div>', unsafe_allow_html=True)
+
 elif mode == "Compare 2 Portfolios":
     st.write("This mode allows you to upload two different portfolios and compare their performance, generating a comprehensive commentary on the differences and insights.")
     st.subheader("Compare Two Portfolios")
